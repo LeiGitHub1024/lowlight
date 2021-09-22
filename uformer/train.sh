d6c12415bbd0e1d1a412b55bde6e60aa60bff048
LeiGitHub1024 <cmcc.coder@gmail.com>
2021-09-22 09:54:15 +0800

    小修改

--- uformer/train.sh
+++ uformer/train.sh
@@ -14,3 +14,11 @@
 #     --train_ps 128 --train_dir /cache/SIDD/train --env 32_0701_1 \
 #     --val_dir /cache/SIDD/val --embed_dim 32 --warmup
 
+
+
+
+
+
+# python3 ./train.py --arch Uformer --batch_size 32 --gpu '0' \
+#     --train_ps 128 --train_dir ../datasets/denoising/sidd/train --env 16_0701_1 \
+#     --val_dir ../datasets/denoising/sidd/train --embed_dim 16 --warmup
